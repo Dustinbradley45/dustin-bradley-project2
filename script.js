152097457a90b3bac5f460b7ebba1cba9160e0df
Dustin <dustinbradley45@live.com>
2019-02-04 09:54:43 -0500

    Finished styling and added most accessibility links

---
+++ script.js
@@ -0,0 +1,22 @@
+//INDEX JS
+
+
+
+
+//BLOG JS
+$(function () { 
+
+   
+
+
+$('.toggleReadMore').on('click', function () {
+    $('.hideTheWords').slideToggle(2000);
+    
+    
+    });
+
+
+
+})
+
+
